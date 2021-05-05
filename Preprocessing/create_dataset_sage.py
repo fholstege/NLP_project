848594cb5454755c5558a794d4d9536a62bf8fc5
fholstege <31654652+fholstege@users.noreply.github.com>
2021-05-05 15:42:39 +0200

    added sleep, added help function

--- Preprocessing/create_dataset_sage.py
+++ Preprocessing/create_dataset_sage.py
@@ -4,6 +4,7 @@
 import urllib.request
 import requests
 import browser_cookie3
+import time
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
@@ -29,6 +30,13 @@ def create_list_urls_sage(list_DOI):
 
     return list_urls_sage
 
+
+def check_items_from_soup_search(ReturnObj):
+    
+    if len(ReturnObj) == 0:
+        return 'NA'
+    else:
+        return ReturnObj[0].text
 
 
 def scrape_article_sage(url, cookies): 
@@ -87,42 +95,28 @@ def scrape_article_sage(url, cookies):
     body = soup.body    
 
     # extract author notes
-    author_notes = soup.find_all("author-notes")
-    
-    if len(author_notes) == 0:
-        author_notes = 'NA'
-    else:
-        author_notes = author_notes[0].text
+    author_notes_search = soup.find_all("author-notes")
+    author_notes = check_items_from_soup_search(author_notes_search)
 
     # get abstract
-    abstract = soup.find_all("abstract")
-    
-    if len(abstract) == 0:
-        abstract = 'NA'
-    else:
-        abstract = abstract[0].text
+    abstract_search = soup.find_all("abstract")
+    abstract = check_items_from_soup_search(abstract_search)
 
-    # extract keywords
-    keywords = soup.find_all("kwd-group")[0].text
+    # extract keywords    
+    keywords_search = soup.find_all("kwd-group")
+    keywords = check_items_from_soup_search(keywords_search)
 
     # extract acknowledgements
-    acknowledge = soup.find_all("ack")
-    
-    if len(acknowledge) == 0:
-        acknowledge = 'NA'
-    else:
-        acknowledge = acknowledge[0].text
+    acknowledge_search = soup.find_all("ack")
+    acknowledge = check_items_from_soup_search(acknowledge_search)
 
     # ref list  - raw HTML
     ref_list = soup.find_all("ref-list")[0]
 
     # fn group (associate editor, declaration of conflicting interest, funding, online supplement)
-    fngroup = soup.find_all("fn-group")
+    fngroup_search = soup.find_all("fn-group")
+    fngroup = check_items_from_soup_search(fngroup_search)
     
-    if len(fngroup) == 0:
-        fngroup = 'NA'
-    else:
-        fngroup = fngroup[0].text
     
     return title, doi, body, author_notes, abstract, keywords, acknowledge, ref_list, fngroup
     
@@ -146,32 +140,41 @@ def create_dataset_sage(list_csv_locations, cookies):
 
     """
     
+    # column names for df to be filled
     col_df = ['title', 'DOI' , 'body', 'author_notes', 'abstract', 'keywords', 'acknowledge', 'ref_list', 'fngroup']
     
+    # list of dfs with data from SAGE
     list_sage_data_per_journals = []
-
+    
+    # go through each csv
     for csv_location in list_csv_locations:
         
+        # get DOI list
         df_WoS = pd.read_csv(csv_location)
-        
         list_DOI = list(df_WoS['DOI'])
         
+        # turn to urls, create df to be filled
         urls_sage = create_list_urls_sage(list_DOI)
         df_sage_data = pd.DataFrame(index = range(1,len(urls_sage)+1), columns = col_df)
         
+        # go through each url and pull the data
         for index_url in range(0,len(urls_sage)):
-            print("{0} / {1}" .format(index_url, len(urls_sage)))
+            print("{0} / {1}" .format(index_url+1, len(urls_sage)))
             
+            # extract data from url
             url = urls_sage[index_url]
-            
             title, doi, body, author_notes, abstract, keywords, acknowledge, ref_list, fngroup = scrape_article_sage(url, cookies)
             
+            # putt data in row and add to df
             row_entry = [title, doi, body, author_notes, abstract, keywords, acknowledge, ref_list, fngroup]
-
             df_sage_data.iloc[index_url] = row_entry
-      
+            
+            # sleep to make sure we are not recognized as DoS attack
+            time.sleep(1)
+        # add the df to list
         list_sage_data_per_journals.append(df_sage_data)
     
+    # return list of df
     return list_sage_data_per_journals
         
         
@@ -181,6 +184,6 @@ def create_dataset_sage(list_csv_locations, cookies):
 cj = browser_cookie3.firefox() 
 
 
-df_journal_of_marketing = create_dataset_sage(['Data/Raw/journalofmarketing.csv'], cj)
+df_journal_of_marketing = create_dataset_sage(['Data/Raw/test.csv'], cj)
 
 
