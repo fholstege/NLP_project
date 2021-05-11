c0a3a86133ee3761cc847bb461e5aa699db93730
muellermarkus <markus.mueller939@gmail.com>
2021-05-11 16:24:14 +0200

    add function to clean scopus and get field data

--- Preprocessing/helpfunctions_preprocessing.py
+++ Preprocessing/helpfunctions_preprocessing.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import time
-
+import re
+import numpy as np
 
 def create_list_urls(list_DOI, base_url):
     """
@@ -92,4 +93,94 @@ def create_dataset_publisher(list_csv_locations, cookies, base_url, scrape_func)
     # return list of df
     return list_data_per_journal
         
+
+def prepare_scopus(data_path, field_data, field_dict):
+    """
+    Function that pulls that cleans the scopus data and combines it with data on journal fields from scimago.
+
+    Parameters
+    ----------
+    data_path : str
+        Path to the scopus data csv.
+    
+    field_data : DataFrame
+        Data on journal names and associated research fields.
         
+    field_dict : Dictionary
+        Data on journal names and associated research fields for easy looping.
+
+    Returns
+    -------
+    output_df : DataFrame
+        Cleaned scopus data and additional variables on the percentage of citations coming from the specified research fields. References that could not be extracted from the reference list or belong to journals that are not found on scimago, books, or newspaper articles, etc. cannot be accounted for.
+
+    """
+    # references
+    scopus_data = pd.read_csv(data_path)
+    
+    # drop unnecessary variables
+    scopus_data.drop(labels = ['Volume', 'Issue', 'Art. No.', 'Page start', 'Page end',
+                  'ISBN', 'CODEN', 'PubMed ID', 'Language of Original Document', 'Page count',
+                  'Abbreviated Source Title', 'Document Type', 'Publication Stage',
+                  'Open Access', 'Source', 'EID', 'Link', 'Affiliations', 'Index Keywords',
+                  'Funding Text 2', 'Editors', 'Publisher', 'ISSN', 'Authors with affiliations',
+                  'Author(s) ID', 'Correspondence Address', 'Funding Text 1', 'Funding Details'],
+        axis = 1, inplace = True)
+
+    # get total number of references for each article
+    scopus_data['num_ref'] = scopus_data['References'].apply(lambda x: str(x).count(";") + 1)
+
+    # get references as columns
+    references = scopus_data['References'].str.split(pat = ";", expand = True)
+
+    # clean references
+    for col in range(references.shape[1]):
+        references[col] = references[col].str.extract(r"\d{4}\)([a-zA-Z0-9_ :]*),")
+
+    # rename columns
+    references = references.add_prefix("ref_")
+
+    # make all lower case before comparison
+    references = references.applymap(lambda s:s.lower() if type(s) == str else s)
+    field_data['journal'] = field_data['journal'].str.lower()
+
+    # remove one duplicate that occurs when putting all journals in lower case
+    field_data = field_data[~field_data.duplicated()]
+
+    # get DOIs
+    references['DOI'] = scopus_data['DOI']
+
+    # replace journals by their respective fields
+    # melt into long format
+    references = references.melt(id_vars = 'DOI', var_name = 'reference', value_name = 'journal')
+
+    # prepare join columns
+    references['journal'] = references['journal'].astype(str).str.strip()
+    field_data['journal'] = field_data['journal'].astype(str).str.strip()
+
+    # left join field data
+    result = pd.merge(references, field_data, how = 'left', on = 'journal', validate = 'many_to_one')
+
+    # clean journal column, code nan as NaN
+    result['journal'] = result['journal'].replace('nan', np.nan) 
+
+    # those not in fields of interest, none, NaN, empty, are NA
+
+    # get back into correct format
+    result = result.pivot(index = 'DOI', columns = 'reference', values = 'field')
+
+    # count fields per journal
+    result = result.apply(pd.Series.value_counts, axis = 1).fillna(0)
+
+    # add count of references found
+    result['refs_found'] = result.sum(axis = 1)
+
+    # merge onto scopus data
+    output_df = scopus_data.merge(result, how = 'left', on = 'DOI', validate = 'many_to_one')
+    output_df['refs_not_found'] = output_df['num_ref'] - output_df['refs_found']
+
+    # get citations in percentages
+    for field in list(field_dict):
+        output_df[field] = output_df[field]/output_df['refs_found']
+        
+    return output_df
---
+++ Preprocessing/prepare_scopus_data.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from helpfunctions_preprocessing import prepare_scopus
+
+# turn scimago data into dataframe to extract fields
+field_dict = {'Marketing' : 'data/raw/marketing_journals.csv', 
+          'AI' :  'data/raw/ai_journals.csv', 
+          'Statistics' : 'data/raw/statistics_journals.csv', 
+          'Sociology / Pol.Sc.' : 'data/raw/socio_political_journals.csv', 
+          'Mgmt. Science / OR' : 'data/raw/mgmtscience_operationsres_journals.csv', 
+          'Economics / Econometrics' : 'data/raw/econ_econometrics_journals.csv',
+          'Psychology' : 'data/raw/psych_journals.csv',
+          'Medicine' : 'data/raw/medicine_journals.csv',
+          'Computer Science' : 'data/raw/cs_journals.csv',
+          'Behavioral Neuroscience' : 'data/raw/behav_neuro_journals.csv',
+          'Org. Behavior / HR Management' : 'data/raw/hr_org_behav_journals.csv',
+          'Mgmt of Tech. and Innovation' : 'data/raw/mgmt_tech_inno_journals.csv',
+          'Business / Int. Management' : 'data/raw/business_int_mgmt_journals.csv',
+          'Mgmt Information Systems' : 'data/raw/mgmt_info_systems_journals.csv',
+          'Strategy and Management' : 'data/raw/strategy_mgmt_journals.csv',
+          'Social Sciences' : 'data/raw/social_sciences_journals.csv'}
+
+# initialise dataframe
+all_journals = pd.DataFrame(columns = ['Rank', 'Title', 'Field', 'ID'])
+
+for field, path in field_dict.items():
+    # input data
+    journals = pd.read_csv(path, sep = ";")
+    # clean frame
+    journals = journals[['Rank', 'Title']]
+    # create field info
+    journals['Field'] = field
+    # merge onto dataframe
+    all_journals = all_journals.append(journals)
+    
+# create unique identifier
+all_journals = all_journals.reset_index()
+all_journals['ID'] = all_journals.index
+    
+# get entries that appear more than once
+df_dupl = all_journals[all_journals['Title'].duplicated(keep = False)]
+
+# sort by rank
+df_dupl = df_dupl.sort_values(by = 'Rank')
+
+# get duplicates to remove from data
+df_dupl = df_dupl[df_dupl['Title'].duplicated(keep = 'first')]
+    
+# remove duplicate titles from other fields
+cond = ~all_journals['ID'].isin(df_dupl['ID'])
+all_journals = all_journals[cond]
+
+# clean to merge onto corpus
+field_data = all_journals[['Title', 'Field']]
+field_data.columns = ['journal', 'field']
+
+
+prepare_scopus('data/raw/journalofmarketing_WoS.csv', field_data, field_dict)
+
+
+    
+# # merge scopus data onto scraped data
+# result = pd.merge(scraped_data, scopus_data, how = 'left', on = 'DOI', sort = False)
+    
+# # remove redundant unnamed column (old index)
+# result.drop(labels = ['Unnamed: 0'], axis = 1, inplace = True)
+
+# # save data
+# result.to_parquet('data/cleaned/' + output_name + '.gzip', compression='gzip')
+    
+# merge_data('data/scraped/journalofmarketing.gzip', 'data/raw/journalofmarketing_WoS.csv', 'jom_merged')
