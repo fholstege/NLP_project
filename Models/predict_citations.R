f086edede549faaea8e140a5a340cb226fb18e50
fholstege <31654652+fholstege@users.noreply.github.com>
2021-06-01 22:09:52 +0200

    Update predict_citations.R

--- Models/predict_citations.R
+++ Models/predict_citations.R
@@ -99,6 +99,10 @@ n_0_log_altmetric = nrow(df_full[log( 1 + df_full$`Altmetric Attention Score`) =
 n_0_log_altmetric/ nrow(df_full[!is.na(df_full$`Altmetric Attention Score` ),])
 
 
+############################
+# 3) LDA topics
+############################
+
 
 # not add topic 3 - since all classified in one of these
 topic_var = c('Topic')
@@ -111,53 +115,101 @@ df_topics <- df_full[,c(dependent_var, topic_var)]
 # check for the entire dataset if the number of citations is zero
 df_topics$citations_is_zero = ifelse(df_topics$`Cited by` == 0, 1, 0)
 
+
 # create dataset where only citations are included that are not zero
-df_model_non_zero <- df_topics[df_topics$`Cited by` != 0,]
+df_model_non_zero_citations <- df_topics[df_topics$`Cited by` != 0,]
+
+## number of citations
 
 # run the binary, logit model for entire dataset
 binary_model_topic = glm(factor(citations_is_zero) ~  factor(Topic), data = na.omit(df_topics %>% select(-`Altmetric Attention Score`)), family = binomial(link = "logit"), control = list(maxit = 50))
 
 # linear model for number of citations >0 
-linear_model_topic = lm(log(1 + `Cited by`) ~ factor(Topic)   , data = na.omit(df_model_non_zero%>% select(-`Altmetric Attention Score`)))
-
+linear_model_topic = lm(log(1 + `Cited by`) ~ factor(Topic)   , data = na.omit(df_model_non_zero_citations%>% select(-`Altmetric Attention Score`)))
 
 summary(binary_model_topic)
 summary(linear_model_topic)
 
+## altmetric interest
+df_topics$altmetric_is_zero = ifelse(df_topics$`Altmetric Attention Score` == 0, 1,0)
+df_topics_non_zero_alt <- df_topics[df_topics$`Altmetric Attention Score` != 0,]
+
+# run the binary, logit model for entire dataset
+binary_model_topic_altmetric = glm(factor(altmetric_is_zero) ~  factor(Topic), data = na.omit(df_topics %>% select(-`Cited by`)), family = binomial(link = "logit"), control = list(maxit = 50))
+
+# linear model for number of altmetric >0 
+linear_model_topic_altmetric = lm(log(1 + `Altmetric Attention Score`) ~ factor(Topic)   , data = na.omit(df_topics_non_zero_alt%>% select(-`Cited by`)))
+
+summary(binary_model_topic_altmetric)
+summary(linear_model_topic_altmetric)
 
 
 
 
+############################
+# 4) Word2vec
+############################
 
 
-df_word2vec = read.csv('../data/representations/word2vec_doc_representation.csv')
-df_word2vec <- df_word2vec %>% select(-X)
-df_word2vec <- na.omit(df_word2vec)
 
-x_vars <- model.matrix(num_ref~. , na.omit(df_word2vec))[,-1]
-y_var <- log(df_word2vec$num_ref)
-lambda_seq <- 10^seq(2, -2, by = -.1)
+df_word2vec_mean_representation = read.csv('../data/representations/word2vec_doc_representation.csv') %>% select(-X) %>% na.omit()
+df_word2vec_max_representation = read.csv('../data/representations/word2vec_doc_representation.csv') %>% select(-X) %>% na.omit()
+df_word2vec_min_representation = read.csv('../data/representations/word2vec_doc_representation.csv')%>% select(-X) %>% na.omit()
+
+
+x_vars_mean <- model.matrix(num_ref~. , na.omit(df_word2vec_mean_representation))[,-1]
+x_vars_max <-  model.matrix(num_ref~. , na.omit(df_word2vec_max_representation))[,-1]
+x_vars_min <- model.matrix(num_ref~. , na.omit(df_word2vec_min_representation))[,-1]
+y_var <- log(df_word2vec_mean_representation)
+
+lambda_cv = 10^seq(2, -2, by = -0.5)
+alpha_cv = seq(0,1,by=0.2)
+
 
 
 # Splitting the data into test and train
 set.seed(123)
-train = sample(1:nrow(x_vars), round(nrow(x_vars)*0.7,0))
-x_test = (-train)
-y_test = y_var[x_test]
+train_indeces = which(df_word2vec_mean_representation$train_set == 1)
+
+# mean representation
+x_vars_mean_train = x_vars_mean[train_indeces,]
+x_vars_mean_test = x_vars_mean[-train_indeces,]
+
+# max representation
+x_vars_max_train = x_vars_max[train_indeces,]
+x_vars_max_test = x_vars_max[-train_indeces,]
+
+# min representation
+x_vars_min_train = x_vars_min[train_indeces,]
+x_vars_min_test = x_vars_min[-train_indeces,]
+
+# dependent variable
+y_var_train = y_var[train_indeces]
+y_var_test = y_var[-train_indeces]
 
 
-sd(y_test)
-
-cv_output <- cv.glmnet(x_vars[train,], y_var[train],
-                       alpha = 1, lambda = lambda_seq, 
+# cv for mean representation
+cv_mean_rep <- cv.glmnet(x_vars_mean_train, y_var_train,
+                       alpha = alpha_cv, lambda = lambda_cv, 
                        nfolds = 5)
 
-opt_lambda <- cv_output$lambda.min
+# cv for max representation
+cv_mean_rep <- cv.glmnet(x_vars_max_train, y_var_train,
+                         alpha = alpha_cv, lambda = lambda_cv, 
+                         nfolds = 5)
+
+cv_min_rep <- cv.glmnet(x_vars_min_train, y_var_train,
+                         alpha = alpha_cv, lambda = lambda_cv, 
+                         nfolds = 5)
+
+
+opt_lambda_mean <- cv_output$lambda.min
+opt_alpha_mean <- cv_output$alpha.min
+
+
 # Rebuilding the model with best lamda value identified
-lasso_model_word2vec <- glmnet(x_vars[train,], y_var[train], alpha = 1, lambda = opt_lambda)
-pred_lasso <- predict(lasso_model_word2vec, s = opt_lambda, newx = x_vars[x_test,])
+lasso_model_mean_word2vec <- glmnet(x_vars_mean_train, y_var_train, alpha = opt_alpha_mean, lambda = opt_lambda_mean)
+pred_elastic_mean <- predict(lasso_model_mean_word2vec, newx = x_vars_mean_test)
 mean((pred - y_test)^2)
 
-baseline_word2vec_model = lm(y_var[train] ~ . , data = data.frame(x_vars[train,]))
-pred_lm = predict(baseline_word2vec_model, newdata =  data.frame(x_vars[x_test,]))
-mean((pred_lm - y_test)^2)
+# add linear regression
